a47dbb0a98331ba65ca1c89e48192588ff173ffa
Rodxuan <loesskd@gmail.com>
2023-11-23 20:52:39 -0400

    Usando el método toJSON que excluye los campos __v y password al convertir el objeto a JSON define un esquema de Mongoose para un usuario con nombre, email, contraseña y rol, exporta el modelo.

---
+++ models/usuario.js
@@ -0,0 +1,29 @@
+const {Schema, model} = require('mongoose')
+
+const UsuarioSchema = Schema({
+
+    nombre:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:'EDITOR'
+    }
+})
+
+UsuarioSchema.method('toJSON', function(){
+  const {__v, password,  ...object} = this.toObject()
+  return object
+})
+
+module.exports = model('Usuario', UsuarioSchema)
